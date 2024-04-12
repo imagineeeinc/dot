89b88d66b88bdf99905837a0f434f4a18bcaae2e
Imagineee <59028351+imagineeeinc@users.noreply.github.com>
2024-04-12 23:35:31 +0100

    added windows support and more neovide options

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,6 +3,33 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
+-- Windows Specific
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  -- Enable powershell as your default shell
+  vim.opt.shell = "pwsh.exe -NoLogo"
+  vim.opt.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.cmd [[
+      let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+      let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+      set shellquote= shellxquote=
+    ]]
+
+  -- Set a compatible clipboard manager
+  vim.g.clipboard = {
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+  }
+end
+
+-- Custom Config start
+
 vim.cmd.colorscheme = "catppuccin"
 lvim.use_icons = true
 lvim.leader = "space"
@@ -263,7 +290,6 @@ lvim.keys.normal_mode["<C-c>"] = ":Gen<CR>"
 lvim.keys.visual_mode["<C-c>"] = ":Gen<CR>"
 
 if vim.g.neovide == true then
-  vim.g.neovide_scale_factor = 0.8
   lvim.builtin.which_key.mappings["+"] = {
     "<cmd>lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", "Scale Neovide Zoom in"
   }
@@ -289,3 +315,15 @@ lvim.keys.normal_mode["<C-s>"] = ":w<CR>"
 --Tabs
 lvim.keys.normal_mode["<S-h>"] = ":bprev<CR>"
 lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
+
+-- Nvide
+if vim.g.neovide == true then
+  vim.g.neovide_scale_factor = 0.8
+  vim.o.guifont = "SpaceMono Nerd Font Regular,Space Mono Regular"
+  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_cursor_smooth_blink = true
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.g.neovide_cursor_vfx_particle_density = 80.0
+end
