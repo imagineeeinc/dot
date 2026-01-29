bd9847d99c70f168d93904a550745754dde84a04
imagineeeinc <imagineee@tilde.green>
2026-01-29 10:31:10 +0000

    bash: added new alias for yazi and fastfetch

--- bash-shared/.bashrc.shared.sh
+++ bash-shared/.bashrc.shared.sh
@@ -3,6 +3,8 @@ alias ll="ls -alF"
 alias lss="eza --icons -lh --tree -L=1 -GF --no-permissions --no-user --git"
 alias lsr="eza --icons -lh --tree --no-permissions --no-user --git"
 alias lz="lazygit"
+alias yz="yazi"
+alias ff="fastfetch"
 alias frees="du -h -d 1 . | sort -hr"
 alias :q="exit"
 
